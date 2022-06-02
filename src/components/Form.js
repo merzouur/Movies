492cee26f031b60df9db684cf8c40bac810e1e08
merzou_r <merzou_r@etna-alternance.net>
2022-06-02 09:24:37 +0200

    deploiement

---
+++ src/components/Form.js
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import Card from './Card';
+
+const Form = () => {
+    const [moviesData, setMoviesData] = useState([])
+    const [search, setSearch] = useState("code")
+
+    useEffect(() => {
+        axios.get('https://api.themoviedb.org/3/search/movie?api_key=f090bb54758cabf231fb605d3e3e0468&query=coucou&language=fr-FR').then((res) => setMoviesData(res.data.results));
+
+    }, [search]);
+
+    return (
+       <div className='form-component'>
+           <div className='form-container'>
+               <form>
+                   <input type="text" placeholder='Entrer le titre' id="search-input" onChange={(e) => setSearch(e.target.value)}/>
+                   <input type="submit" value="Rechercher"/>
+                </form>
+                <div className='btn-sort-container'>
+                    <div className='btn-sort' id="goodToBad">
+                        Top<span>↦</span>
+                    </div>
+                    <div className='btn-sort' id="badToGood">
+                        Flop<span>↦</span>
+                    </div>
+                </div>
+            </div>
+            <div className='result'>
+                {moviesData.slice(0, 12).map((movie) =>( 
+                <Card key={movie.id} movie={movie}/>
+                ))}
+                </div>
+        </div>
+    );
+};
+
+export default Form;
